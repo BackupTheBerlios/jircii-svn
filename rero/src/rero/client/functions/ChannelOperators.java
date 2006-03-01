28c1a6f5431c7b0621a08c9343774e01ae5ec896
crazycoder <crazycoder@14983b49-aaf6-0310-98ab-aa9e6cf7ec92>
2006-03-01 16:47:40 +0000

    - Fixed modes in nicklist and status bar (all possible channel modes   should be correctly supported now, including &, ~, .) (Serge) - Fixed &getModeFor returning 2 characters in some cases. According   to docs it should always return the highest channel mode user has   as a single character (Serge) - Added @ &getModeChars function, returns channel mode chars supported   by the server (@,%,+,&,~,.) (Serge)

--- rero/src/rero/client/functions/ChannelOperators.java
+++ rero/src/rero/client/functions/ChannelOperators.java
@@ -1,205 +1,189 @@
 package rero.client.functions;
 
-import sleep.engine.*;
-import sleep.runtime.*;
-import sleep.interfaces.*;
-
-import rero.client.*;
-import rero.ircfw.data.*;
-import rero.ircfw.*;
-
+import rero.client.Feature;
+import rero.ircfw.Channel;
+import rero.ircfw.InternalDataList;
 import sleep.bridges.BridgeUtilities;
+import sleep.interfaces.Function;
+import sleep.interfaces.Loadable;
+import sleep.interfaces.Predicate;
+import sleep.runtime.Scalar;
+import sleep.runtime.ScriptInstance;
+import sleep.runtime.SleepUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
 
-public class ChannelOperators extends Feature implements Predicate, Function, Loadable
-{
-   protected InternalDataList data;
- 
-   public void init()
-   {
-      getCapabilities().getScriptCore().addBridge(this);
-      
-      data = (InternalDataList)getCapabilities().getDataStructure("clientInformation");
-   }
+public class ChannelOperators extends Feature implements Predicate, Function, Loadable {
+  protected InternalDataList data;
 
-   public boolean scriptLoaded(ScriptInstance script)
-   {
-      String[] contents = new String[] { 
-          "ison",      /** predicates */
-          "isop",
-          "ishalfop",
-          "isvoice",
-          "isnormal",
-          "ismode",
-          "hasmode",
-          "&getUsers",  /** functions that return an array value */
-          "&getOps",
-          "&getHalfOps",
-          "&getVoiced",
-          "&getNormal",
-          "&getTopic",  /** functions that return a normal scalar value */
-          "&getKey",
-          "&getMode",
-          "&getLimit",
-          "&getModeFor"
-      };
+  public void init() {
+    getCapabilities().getScriptCore().addBridge(this);
 
-      for (int x = 0; x < contents.length; x++)
-      {
-         script.getScriptEnvironment().getEnvironment().put(contents[x], this);
-      }       
+    data = (InternalDataList) getCapabilities().getDataStructure("clientInformation");
+  }
 
-      return true;
-   }
+  public boolean scriptLoaded(ScriptInstance script) {
+    String[] contents = new String[]{
+      "ison",      /** predicates */
+      "isop",
+      "ishalfop",
+      "isvoice",
+      "isnormal",
+      "ismode",
+      "hasmode",
+      "&getUsers",  /** functions that return an array value */
+      "&getOps",
+      "&getHalfOps",
+      "&getVoiced",
+      "&getNormal",
+      "&getTopic",  /** functions that return a normal scalar value */
+      "&getKey",
+      "&getMode",
+      "&getLimit",
+      "&getModeFor",
+      "&getModeChars"
+    };
 
-   public boolean scriptUnloaded(ScriptInstance script)
-   {
-      return true;
-   }
+    for (int x = 0; x < contents.length; x++) {
+      script.getScriptEnvironment().getEnvironment().put(contents[x], this);
+    }
 
-   public Scalar evaluate(String function, ScriptInstance script, Stack locals)
-   {
-      if (function.equals("&getModeFor"))
-      {
-          String  _nick    = BridgeUtilities.getString(locals, "");
-          String  _channel = BridgeUtilities.getString(locals, "");
-          Channel channel  = data.getChannel(_channel);
- 
-          if (data.getUser(_nick) == null)
-              return SleepUtils.getEmptyScalar();
+    return true;
+  }
 
-          return SleepUtils.getScalar(data.getPrefixInfo().toString(data.getUser(_nick).getModeFor(channel)));
+  public boolean scriptUnloaded(ScriptInstance script) {
+    return true;
+  }
+
+  public Scalar evaluate(String function, ScriptInstance script, Stack locals) {
+    if (function.equals("&getModeChars")) {
+      String modes = data.getPrefixInfo().getModes();
+      char[] chars = modes.toCharArray();
+      ArrayList ch = new ArrayList(chars.length);
+      for (int i = 0; i < chars.length; i++) {
+        char mchar = chars[i];
+        ch.add(String.valueOf(mchar));
       }
+      return SleepUtils.getArrayWrapper(ch);
+    }
 
-      if (locals.size() != 1)
-      {
-         return SleepUtils.getEmptyScalar();
-      }
-
-      String  _channel = ((Scalar)locals.pop()).getValue().toString();
+    if (function.equals("&getModeFor")) {
+      String _nick = BridgeUtilities.getString(locals, "");
+      String _channel = BridgeUtilities.getString(locals, "");
       Channel channel = data.getChannel(_channel);
-  
-      if (channel == null)
-      {
-         return null;
-      }
 
-      if (function.equals("&getTopic"))
-      {
-         return SleepUtils.getScalar(channel.getTopic());
-      }
+      if (data.getUser(_nick) == null)
+        return SleepUtils.getEmptyScalar();
 
-      if (function.equals("&getMode"))
-      {
-         return SleepUtils.getScalar(channel.getMode().toString());
-      }
+      return SleepUtils.getScalar(data.getPrefixInfo().toString(data.getUser(_nick).getModeFor(channel)));
+    }
 
-      if (function.equals("&getKey"))
-      {
-         return SleepUtils.getScalar(channel.getKey());
-      }
+    if (locals.size() != 1) {
+      return SleepUtils.getEmptyScalar();
+    }
 
-      if (function.equals("&getLimit"))
-      {
-         return SleepUtils.getScalar(channel.getLimit());
-      }
+    String _channel = ((Scalar) locals.pop()).getValue().toString();
+    Channel channel = data.getChannel(_channel);
 
-      Stack rv = new Stack();
-      Iterator i = null;
-
-      if (function.equals("&getUsers"))
-      {
-         return SleepUtils.getArrayWrapper(channel.getAllUsers());
-      }
-
-      if (function.equals("&getOps"))
-      {
-         return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'o'));
-      }
-
-      if (function.equals("&getHalfOps"))
-      {
-         return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'h'));
-      }
-
-      if (function.equals("&getVoiced"))
-      {
-         return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'v'));
-      }
-
-      if (function.equals("&getNormal"))
-      {
-         return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, ' '));
-      }
-
+    if (channel == null) {
       return null;
-   }
+    }
 
-   public boolean decide(String predicate, ScriptInstance script, Stack terms)
-   {
-      if (terms.size() != 2)
-      {
-         return false;
-      }
+    if (function.equals("&getTopic")) {
+      return SleepUtils.getScalar(channel.getTopic());
+    }
 
-      String channel = ((Scalar)terms.pop()).getValue().toString();  
-      String nick    = ((Scalar)terms.pop()).getValue().toString();
- 
-      if (data.getChannel(channel) == null)
-      {
-         return false;
-      }
+    if (function.equals("&getMode")) {
+      return SleepUtils.getScalar(channel.getMode().toString());
+    }
 
-      if (predicate.equals("ismode")) 
-      {
-         for (int x = 0; x < nick.length(); x++)
-         {
-            if (! data.getChannel(channel).getMode().isSet(nick.charAt(x)) )
-            {
-               return false;
-            }
-         }
-         return true;
-      }
+    if (function.equals("&getKey")) {
+      return SleepUtils.getScalar(channel.getKey());
+    }
 
-      if (!data.isUser(nick))
-      {
-         return false;
-      }
+    if (function.equals("&getLimit")) {
+      return SleepUtils.getScalar(channel.getLimit());
+    }
 
-      if (predicate.equals("ison"))
-      {
-         return data.isOn(data.getUser(nick), data.getChannel(channel));
-      } 
+    Stack rv = new Stack();
+    Iterator i = null;
 
-      int temp = data.getUser(nick).getModeFor(data.getChannel(channel));
-     
-      if (predicate.equals("isop"))
-      {
-         return data.getPrefixInfo().isMode(temp, 'o');
-      }
+    if (function.equals("&getUsers")) {
+      return SleepUtils.getArrayWrapper(channel.getAllUsers());
+    }
 
-      if (predicate.equals("ishalfop"))
-      {
-         return data.getPrefixInfo().isMode(temp, 'h');
-      }
+    if (function.equals("&getOps")) {
+      return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'o'));
+    }
 
-      if (predicate.equals("isvoice"))
-      {
-         return data.getPrefixInfo().isMode(temp, 'v');
-      }
+    if (function.equals("&getHalfOps")) {
+      return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'h'));
+    }
 
-      if (predicate.equals("isnormal"))
-      {
-         return temp == 0;
-      }
+    if (function.equals("&getVoiced")) {
+      return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, 'v'));
+    }
 
-      if (predicate.equals("hasmode"))
-      {
-         return temp != 0;
-      }
+    if (function.equals("&getNormal")) {
+      return SleepUtils.getArrayWrapper(data.getUsersWithMode(_channel, ' '));
+    }
 
+    return null;
+  }
+
+  public boolean decide(String predicate, ScriptInstance script, Stack terms) {
+    if (terms.size() != 2) {
       return false;
-   }
+    }
+
+    String channel = ((Scalar) terms.pop()).getValue().toString();
+    String nick = ((Scalar) terms.pop()).getValue().toString();
+
+    if (data.getChannel(channel) == null) {
+      return false;
+    }
+
+    if (predicate.equals("ismode")) {
+      for (int x = 0; x < nick.length(); x++) {
+        if (! data.getChannel(channel).getMode().isSet(nick.charAt(x))) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (!data.isUser(nick)) {
+      return false;
+    }
+
+    if (predicate.equals("ison")) {
+      return data.isOn(data.getUser(nick), data.getChannel(channel));
+    }
+
+    int temp = data.getUser(nick).getModeFor(data.getChannel(channel));
+
+    if (predicate.equals("isop")) {
+      return data.getPrefixInfo().isMode(temp, 'o');
+    }
+
+    if (predicate.equals("ishalfop")) {
+      return data.getPrefixInfo().isMode(temp, 'h');
+    }
+
+    if (predicate.equals("isvoice")) {
+      return data.getPrefixInfo().isMode(temp, 'v');
+    }
+
+    if (predicate.equals("isnormal")) {
+      return temp == 0;
+    }
+
+    if (predicate.equals("hasmode")) {
+      return temp != 0;
+    }
+
+    return false;
+  }
 }
--- rero/src/rero/ircfw/UserMode.java
+++ rero/src/rero/ircfw/UserMode.java
@@ -1,101 +1,96 @@
 package rero.ircfw;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-/** This class is a mode interpreter for each server.  Basically each server can have channel user prefix modes.
-    The state of these modes is stored as an int within each User object (associated in a hashmap with the Channel
-    object as the key).  This class is used to make sense of the integer mode value for a given servers configuration */
-public class UserMode
-{
-   private String modes; // the actual mode characters i.e. ov
-   private String chars; // the actual display characters i.e. @+ 
+/**
+ * This class is a mode interpreter for each server.  Basically each server can have channel user prefix modes. The
+ * state of these modes is stored as an int within each User object (associated in a hashmap with the Channel object as
+ * the key).  This class is used to make sense of the integer mode value for a given servers configuration
+ */
+public class UserMode {
+  private String modes; // the actual mode characters i.e. ov
+  private String chars; // the actual display characters i.e. @+
 
-   public UserMode(String m, String c)
-   {
-      modes = m;
-      chars = c;
-   }
+  public UserMode(String m, String c) {
+    modes = m;
+    chars = c;
+  }
 
-   public Set getUsersWithMode(Channel channel, char mode)
-   {
-      Set copy = new LinkedHashSet();
-      Iterator i = channel.getAllUsers().iterator();
-      while (i.hasNext())
-      {
-         User temp = (User)i.next();
-         int  m    = temp.getModeFor(channel);
- 
-         if (isMode(m, mode))
-         {
-            copy.add(temp);
-         }
+  public Set getUsersWithMode(Channel channel, char mode) {
+    Set copy = new LinkedHashSet();
+    Iterator i = channel.getAllUsers().iterator();
+    while (i.hasNext()) {
+      User temp = (User) i.next();
+      int m = temp.getModeFor(channel);
+
+      if (isMode(m, mode)) {
+        copy.add(temp);
       }
+    }
 
-      return copy;
-   }
+    return copy;
+  }
 
-   public boolean isPrefixMode(char m)
-   {
-      return modes.indexOf(m) > -1;
-   }
+  public boolean isPrefixMode(char m) {
+    return modes.indexOf(m) > -1;
+  }
 
-   public boolean isPrefixChar(char m)
-   {
-       return chars.indexOf(m) > -1;
-   }
+  public boolean isPrefixChar(char m) {
+    return chars.indexOf(m) > -1;
+  }
 
-   public char getDisplayForMode(char mode)
-   {
-      return chars.charAt(modes.indexOf(mode));
-   }
-    
-   public char getModeForDisplay(char display)
-   {
-      return modes.charAt(chars.indexOf(display));
-   }
+  public char getDisplayForMode(char mode) {
+    return chars.charAt(modes.indexOf(mode));
+  }
 
-   public int getValueFor(char mchar)
-   {
-      return 1 << (modes.length() - modes.indexOf(mchar));
-   }
+  public char getModeForDisplay(char display) {
+    return modes.charAt(chars.indexOf(display));
+  }
 
-   public int setMode(int original, char mchar)
-   {
-      int value = getValueFor(mchar);
-      return original | value;
-   }
+  public int getValueFor(char mchar) {
+    return 1 << (modes.length() - modes.indexOf(mchar));
+  }
 
-   public int unsetMode(int original, char mchar)
-   {
-      int value = getValueFor(mchar);
-      return original & ~(value);
-   }
+  public int setMode(int original, char mchar) {
+    int value = getValueFor(mchar);
+    return original | value;
+  }
 
-   public boolean isMode(int original, char mchar)
-   {
-      if (mchar == ' ' && original == 0)
-            return true;
+  public int unsetMode(int original, char mchar) {
+    int value = getValueFor(mchar);
+    return original & ~(value);
+  }
 
-      int value = getValueFor(mchar);
-      return ((original & value) == value);
-   }
+  public boolean isMode(int original, char mchar) {
+    if (mchar == ' ' && original == 0)
+      return true;
 
-   public String toString(int original)
-   {
-      StringBuffer rv = new StringBuffer();
-      for (int x = 0; x < modes.length(); x++)
-      {
-         if (isMode(original, modes.charAt(x)))
-         {
-            rv.append(chars.charAt(x));
-         }
+    int value = getValueFor(mchar);
+    return ((original & value) == value);
+  }
+
+
+  public String getModes() {
+    return modes;
+  }
+
+  public String getChars() {
+    return chars;
+  }
+
+  public String toString(int original) {
+    for (int x = 0; x < modes.length(); x++) {
+      if (isMode(original, modes.charAt(x))) {
+        return String.valueOf(chars.charAt(x));
       }
+    }
+    return "";
+  }
 
-      return rv.toString();
-   }
+  public String toString() {
+    return "[UCM:(" + modes + ")" + chars + "]";
+  }
 
-   public String toString()
-   {
-      return "[UCM:("+modes+")"+chars+"]";
-   }
 }
